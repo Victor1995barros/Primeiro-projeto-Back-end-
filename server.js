b751a4aaf75bff9807a3c5a5dd31faf8d9a2078f
Victor1995barros <victor.vbarros@gmail.com>
2024-11-20 12:33:40 -0300

    Terceiro dia

--- server.js
+++ server.js
@@ -1,41 +1,12 @@
 import express from "express";
+import routes from "./src/config/routes/postsRoutes.js";
 
-const posts = [
-    { 
-        descricao: 'Uma foto teste', 
-        imagem: 'https://placecat.com/millie/300/150', 
-        id: 1 
-    },
-    { 
-        descricao: 'Uma gato fazendo yoga', 
-        imagem: 'https://placecat.com/millie/300/150', 
-        id: 2 
-    },
-    { 
-        descricao: 'Uma gato fazendo panqueca', 
-        imagem: 'https://placecat.com/millie/300/150', 
-        id: 3 
-    },
-  ];
 
 const app = express();
-app.use(express.json());
+routes(app)
 
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
 
-app.get("/posts", (req, res) => {
-    res.status(200).json(posts);
-});
 
-function buscarPostPorId(id){
-    return posts.findIndex((post) => {
-        return post.id === Number(id);
-    });
-}
-
-app.get("/posts/:id", (req, res) => {
-    const index = buscarPostPorId(req.params.id)
-    res.status(200).json(posts[index]);
-});
